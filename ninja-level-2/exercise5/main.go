1ed8b8b9a0e67e3135a863a8484185d5fc11ab23
Marcos Ribeiro <marcosgrsouto@gmail.com>
2022-03-24 15:14:32 -0300

    Exercise 5 solved

---
+++ ninja-level-2/exercise5/main.go
@@ -0,0 +1,14 @@
+//Create a variable of type string using a raw string literal. Print it.
+
+package main
+
+import "fmt"
+
+func main() {
+	myStr := `BLA HERE
+	
+	BLA THERE
+	
+	BLA HERE`
+	fmt.Println(myStr)
+}
